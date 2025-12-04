0c0a645d58cbe63f4644ef7dc64a3af45997cf61
iso <iso@ema.il>
2025-12-04 20:07:44 +0100

    Experiments with a Grasshopper component

---
+++ GreenFDT/GH_Terrain.cs
@@ -0,0 +1,49 @@
+using Rhino.Geometry;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+
+namespace GreenFDT;
+public class GH_TerrainItem
+{
+    public string Name { get; set; }
+    public Point3 Position { get; set; }
+    public Vector3 Size { get; set; }
+
+    public BoxGroup(string name, Point3 position, Vector3 size)
+    {
+        Name = name;
+        Position = position;
+        Size = size;
+    }
+
+    public override string ToString() => $"{Name} as {Position} sized {Size}";
+}
+
+public class GH_Terrain : GH_Goo<GH_TerrainItem>
+{
+    public GH_Terrain() : base() { }
+    public GH_Terrain(GH_TerrainItem group) : base(group) { }
+
+    public override IGH_Goo Duplicate() => new GH_Terrain(Value);
+
+    public override bool IsValid => Value != null;
+
+    public override string TypeName => "Terrain";
+    public override string TypeDescription => "A group of terain items";
+
+    public override string ToString()
+    {
+        return Value?.ToString() ?? "Null Terrain";
+    }
+}
+
+public class Param_Terrain : GH_Param<GH_Terrain>
+{
+    public Param_Terrain()
+      : base("Terrain", "Terrain",
+             "One or more terrain items",
+             "Geometry", "Primitive",
+             GH_ParamAccess.item) { }
+
+    public override Guid ComponentGuid => new Guid("YOUR-GUID-HERE");
+}
