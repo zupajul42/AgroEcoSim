a632ed0c5ba3da39446a361eae9b20581591ff86
ZupaJul <44403676+juiian7@users.noreply.github.com>
2026-04-09 21:39:13 +0200

    Add some explanation and comments

--- WebFrontend/src/components/designer/Timeline.tsx
+++ WebFrontend/src/components/designer/Timeline.tsx
@@ -3,7 +3,8 @@ import state from "../../pages/Designer/DesignerState";
 export function Timeline() {
     return (
         <div>
-            <input type="number" onInput={(e) => (state.time.value = (e.target as any)?.value)} />
+            Day:
+            <input type="number" value={0} min={0} onInput={(e) => (state.time.value = (e.target as any)?.value)} />
         </div>
     );
 }
